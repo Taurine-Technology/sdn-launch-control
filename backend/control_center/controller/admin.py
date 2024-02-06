734464119429569e1193f37066d726b86eee02f8
Keegan White <keeganthomaswhite@gmail.com>
2024-02-06 16:01:27 +0200

    registered model for Django admin page

--- backend/control_center/controller/admin.py
+++ backend/control_center/controller/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Controller
+
+
+@admin.register(Controller)
+class ControllerAdmin(admin.ModelAdmin):
+    list_display = ('lan_ip_address', 'type', 'device')

5c82fd6738d4c0b1241aeffdc168a8ce851d45fd
Keegan White <keeganthomaswhite@gmail.com>
2024-04-24 10:03:13 +0200

    added web socket support

--- backend/control_center/control_center/asgi.py
+++ backend/control_center/control_center/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for control_center project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import control_center.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_center.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            control_center.routing.websocket_urlpatterns
+        )
+    ),
+})
